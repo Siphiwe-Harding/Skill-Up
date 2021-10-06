cca4abc3d0226713fa9bb8a14f2086e3f81a0bfc
Siphiwe Dlali <Siphiwe.Dlali@investec.co.za>
2021-10-06 12:51:35 +0200

    Add project files.

---
+++ MusicApi2/Controllers/SongsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicApi2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private static List<Song> songs = new List<Song>()
+        {
+            new Song(){id = 0, title = "Willow", language = "English"},
+            new Song(){id = 1, title = "After Glow", language = "English"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return songs;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Song  song)
+        {
+            songs.Add(song);
+        }
+    }
+}
---
+++ MusicApi2/Models/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi2.Models
+{
+    public class Song
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string language { get; set; }
+    }
+}
